57dfd12a63cc956fdccc1d8861b3b60e084d9702
Mingmar Gyalzen Sherpa <mingmagsherpa6@gmail.com>
2024-07-09 15:47:35 +0545

    create user route

--- src/routes/index.routes.ts
+++ src/routes/index.routes.ts
@@ -1,9 +1,12 @@
 import todoRouter from "./todo.routes";
+import userRouter from "./user.routes";
 
 import express from "express";
 
 const router = express();
 
 router.use("/todos", todoRouter);
+router.use("/user", userRouter);
+
 
 export default router;
